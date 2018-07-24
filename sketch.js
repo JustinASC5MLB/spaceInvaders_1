0f225c2a3cc6b50989756b9edfd2461e1d526872
RiVayne <justintleong123@gmail.com>
2018-07-24 16:22:30 -0400

    message

--- sketch.js
+++ sketch.js
@@ -1,14 +1,18 @@
 function setup(){
     createCanvas(1350, 787);
+    bx = playershipx;
+    by = playershipy;
 }
-const playershipx = 650;
-const playershipy = 700;
-let a = 50;
-let b = 250;
-let va = 2.5;
-let bulletx = 650;
-let bullety = 700;
-let bulletspeed = 0;
+let playershipx = 650;
+let playershipy = 700;
+let bullet = false;
+let bx,by; //bulletx, bullety
+function checkForCollision(){
+    const enemyx = 20;
+    const enemyy = 20;
+    
+}
+
 
 function draw() {
     background("black");
@@ -33,7 +37,13 @@ function draw() {
     rect(900, 200, 50, 50)
     rect(1000, 200, 50, 50)
     rect(1100, 200, 50, 50)
- 
+
+    if (dist(200, 100, bx, by) <= 35) {
+        console.log("hi")
+    }
+
+
+
 
     if (keyIsDown(LEFT_ARROW)) {
         playershipx -= 5;
@@ -44,43 +54,22 @@ function draw() {
     }  
 if (playershipx <= 0) {
     playershipx = -playershipx
-
+}
 if (playershipx >= 1300){
     playershipx = -playershipx
 }
 fill(255, 0, 0);
 rect(playershipx, playershipy, 50, 50);
+    if (bullet === true){
+        fill("pink")
+        by = by - 20;
+        rect (bx, by, 20, 20)
+    } 
 }
 function keyPressed(){
-    if (){}
-a += va;
-if(a >= 1300){
-    va = -va;
-    fill('white')
+    if (keyCode === 32){
+        bullet = true;
+        bx = playershipx + 25;
+        by = playershipy - 50;
     }
-if(a <= 5){
-    va = -va;
-    fill('white') 
-}    
-
-    rect(200, 100, 50, 50)
-    rect(300, 100, 50, 50)
-    rect(400, 100, 50, 50)
-    rect(500, 100, 50, 50)
-    rect(600, 100, 50, 50)
-    rect(700, 100, 50, 50)
-    rect(800, 100, 50, 50)
-    rect(900, 100, 50, 50)
-    rect(1000, 100, 50, 50)
-    rect(1100, 100, 50, 50)
-    rect(200, 200, 50, 50)
-    rect(300, 200, 50, 50)
-    rect(400, 200, 50, 50)
-    rect(500, 200, 50, 50)
-    rect(600, 200, 50, 50)
-    rect(700, 200, 50, 50)
-    rect(800, 200, 50, 50)
-    rect(900, 200, 50, 50)
-    rect(1000, 200, 50, 50)
-    rect(1100, 200, 50, 50)
 }
